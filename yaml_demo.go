655d9c24be7c9cebcbd68d34f892ba1e73aa2443
ggivl <ggivler.gmail.com>
2025-08-13 18:20:27 -0400

    Added new_csv_output_full.csv and added methods to yaml_demo.go to read and write csv files.

--- yaml_demo.go
+++ yaml_demo.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"gopkg.in/yaml.v3"
+	"io"
 	"io/ioutil"
 	"log"
+	"os"
+	"strings"
 )
 
 // Config represents the structure of config.yml
@@ -97,6 +101,72 @@ func writeConfig(config Config, filename string) Config {
 	return config
 }
 
+func readCSV(filename string) [][]string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		log.Fatalf("Error opening file: %v\n", err)
+		return nil
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		log.Fatalf("Error reading file: %v\n", err)
+		return nil
+	}
+
+	fmt.Printf("Read %d records\n", len(records))
+	for i, record := range records {
+		fmt.Printf("Record %d: %v\n", i+1, record)
+		// Also show as comma-separated values:
+		fmt.Printf("  CSV format: %s\n", strings.Join(record, ","))
+
+	}
+
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		fmt.Printf("Error seeking file: %v\n", err)
+		log.Fatalf("Error seeking file: %v\n", err)
+		return nil
+	}
+
+	reader = csv.NewReader(file)
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Error reading file: %v\n", err)
+			log.Fatalf("Error reading file: %v\n", err)
+			return nil
+		}
+		fmt.Println(strings.Join(record, ","))
+	}
+	return records
+}
+
+func writeCSV(filename string, records [][]string) {
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	err = writer.WriteAll(records)
+	if err != nil {
+		fmt.Printf("Error writing file: %v\n", err)
+	}
+	log.Printf("Wrote %d records to the file %s\n", len(records), filename)
+}
+
 // Main function to run the YAML test
 func main() {
 	fmt.Println("Testing YAML configuration parsing...")
@@ -108,4 +178,7 @@ func main() {
 	conf.FilePaths.MusicLibraryPath = newMusicLibraryPath
 	new_conf := writeConfig(conf, "new_config.yml")
 	fmt.Printf("New Music Library Path %s\n", new_conf.FilePaths.MusicLibraryPath)
+	records := readCSV("csv_output_full.csv")
+	fmt.Printf("Found %d Records in CSV file %s\n", len(records), "csv_output_full.csv")
+	writeCSV("new_csv_output_full.csv", records)
 }
